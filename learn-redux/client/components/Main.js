725c138efb99461cee5c4e69a4366aa3e8193995
samjeffcoat <samjeffcoat@gmail.com>
2019-02-02 15:41:36 -0600

    Creates a component for Single.js

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const Main = React.createClass({
-    render(){
-        return(
-            <div>
-                <h1>
-                    <Link to="/" >  Reduxstagram</Link>  
-                    </h1>
-            </div>
-        )
-    }
-})
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="/"> Samstagram</Link>
+        </h1>
+      </div>
+    );
+  }
+});
 
 export default Main;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,9 +1,12 @@
 // let's go!
-import React from  'react';
+import React from 'react';
 
-import {render } from 'react-dom';
+import { render } from 'react-dom';
 
 // Import Css
 import css from './styles/style.styl';
 
-render(<p>Hi</p>, document.getElementById('root'));
+// Import Components
+import Main from './components/Main';
+
+render(<Main />, document.getElementById('root'));
