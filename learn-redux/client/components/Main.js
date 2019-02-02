255d58332b84399d7f212fe779182ed9309725a0
samjeffcoat <samjeffcoat@gmail.com>
2019-02-02 12:37:36 -0600

    Adds Main.js with a link tag and prettier

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+
 const Main = React.createClass({
     render(){
         return(
             <div>
-                <h1>Reduxstagram</h1>
+                <h1>
+                    <Link to="/" >  Reduxstagram</Link>  
+                    </h1>
             </div>
         )
     }
 })
+
+export default Main;
