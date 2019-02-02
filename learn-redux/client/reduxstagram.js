2b5f20b6eaafe0ff5c88fe5f4a73d59f395a8e96
samjeffcoat <samjeffcoat@gmail.com>
2019-02-02 12:22:18 -0600

    Sets up project

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,9 @@
 // let's go!
+import React from  'react';
+
+import {render } from 'react-dom';
+
+// Import Css
+import css from './styles/style.styl';
+
+render(<p>Hi</p>, document.getElementById('root'));
